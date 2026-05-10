e175a0b755ce3aa088172154af3d6213eadc32f7
walubo <magtoaries799@gmail.com>
2026-05-10 17:31:05 +0800

    new api
    POS V2

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -10,11 +10,22 @@ return Application::configure(basePath: dirname(__DIR__))
         commands: __DIR__.'/../routes/console.php',
         health: '/up',
     )
-    ->withMiddleware(function (Middleware $middleware): void {
+    ->withMiddleware(function (Middleware $middleware) {
+        // This allows Postman to bypass CSRF protection (fixes 419 error)
+        $middleware->validateCsrfTokens(except: [
+            '/register',
+            '/login',
+            '/categories',
+            '/products',
+            '/sales',
+            '/order-items',
+        ]);
+
+        // This registers your custom Role middleware
         $middleware->alias([
             'role' => \App\Http\Middleware\RoleMiddleware::class,
         ]);
     })
-    ->withExceptions(function (Exceptions $exceptions): void {
+    ->withExceptions(function (Exceptions $exceptions) {
         //
     })->create();
