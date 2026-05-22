6b73fbaf5f92c8a42dbb2da1e61bda7854c6acac
walubo <magtoaries799@gmail.com>
2026-05-23 01:55:39 +0800

    small change

--- start_servers.sh
+++ start_servers.sh
@@ -15,7 +15,7 @@ php artisan optimize:clear
 # Start development servers concurrently
 exec npx concurrently \
   -c "#93c5fd,#c4b5fd,#fb7185" \
-  "(cd public && exec /home/ariesu_linux/local-php/php -S 127.0.0.1:8000 ../vendor/laravel/framework/src/Illuminate/Foundation/resources/server.php)" \
+  "php -d display_errors=0 artisan serve --host=127.0.0.1 --port=8000" \
   "php artisan queue:listen --tries=1 --timeout=0" \
   "npm run dev" \
   --names="server,queue,vite" \
